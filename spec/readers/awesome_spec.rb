42759c1efddd98bbb241caa72267552be91c9825
quephird <danielle_kefford@hotmail.com>
2017-01-28 02:58:41 -0500

    Included BeginBlock reader to spec for Awesome reader.

--- spec/readers/awesome_spec.rb
+++ spec/readers/awesome_spec.rb
@@ -1,3 +1,4 @@
+require_relative '../../readers/begin_block'
 require_relative '../../readers/constant'
 require_relative '../../readers/identifier'
 require_relative '../../readers/number'
@@ -7,6 +8,7 @@ require_relative '../../readers/sequence'
 require_relative '../../readers/awesome'
 
 describe Readers::Awesome do
+  let(:begin_block_reader) { Readers::BeginBlock.new }
   let(:constant_reader) { Readers::Constant.new }
   let(:identifier_reader) { Readers::Identifier.new }
   let(:number_reader) { Readers::Number.new }
@@ -14,6 +16,7 @@ describe Readers::Awesome do
   let(:string_reader) { Readers::String.new }
   let(:sequence_reader) do
      Readers::Sequence.new [
+       begin_block_reader,
        constant_reader,
        identifier_reader,
        number_reader,
@@ -25,13 +28,15 @@ describe Readers::Awesome do
 
   context 'input string contains a class definition' do
     it 'should properly parse the code' do
-      input_code = "class Foo    def foo   42  \"foo\""
+      input_code = "class Foo:\n  def foo:\n    42    \"foo\""
       results = awesome_reader.read(input_code)
       expect(results[:tokens]).to eq [
         [:CLASS, 'class'],
         [:CONSTANT, 'Foo'],
+        [:INDENT, 2],
         [:DEF, 'def'],
         [:IDENTIFIER, 'foo'],
+        [:INDENT, 4],
         [:NUMBER, 42],
         [:STRING, 'foo']
       ]
