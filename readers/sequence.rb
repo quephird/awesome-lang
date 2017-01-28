bb7d53fa3b01d6827fc2e3eb7ac815e16b2b84d7
quephird <danielle_kefford@hotmail.com>
2017-01-28 16:19:37 -0500

    Vastly simplified looping in Sequence reader.

--- readers/sequence.rb
+++ readers/sequence.rb
@@ -7,25 +7,13 @@ module Readers
     def read(stream, results)
       readers = @readers
 
-      loop do
-        if readers.empty?
-          # We've gone through all the readers
+      for reader in readers
+        test_results = reader.read(stream, results)
+
+        if test_results[:match_found]
+          # # We got a match, stop looping.
+          results = test_results
           break
-        else
-          # Attempt to read with the first reader in sequence of readers.
-          (reader, *rest_readers) = readers
-
-          # Read with the first reader.
-          test_results = reader.read(stream, results)
-
-          if test_results[:match_found]
-            # We got a match; empty out the readers array so we stop looping.
-            readers = []
-            results = test_results
-          else
-            # We didn't get a match; move onto the next reader, preserving the results.
-            readers = rest_readers
-          end
         end
       end
 
