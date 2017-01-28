6739e830b0122151188c503783dbff5ba73cacbd
quephird <danielle_kefford@hotmail.com>
2017-01-28 16:37:56 -0500

    Simplified looping in Awesome reader as well.

--- readers/awesome.rb
+++ readers/awesome.rb
@@ -14,19 +14,12 @@ module Readers
         remaining_code: stream
       }
 
-      loop do
-        remaining_code = results[:remaining_code]
+      while !(remaining_code = results[:remaining_code]).empty?
+        test_results = @sequence_reader.read(remaining_code, results)
 
-        if remaining_code.empty?
-          # We've gone through all the code; we're done.
-          break
-        else
-          test_results = @sequence_reader.read(remaining_code, results)
-
-          if !test_results[:match_found]
-            # We didn't get a match; we have a problem.
-            raise "Parse error at line #{}, column #{}"
-          end
+        if !test_results[:match_found]
+          # We didn't get a match; we have a problem.
+          raise "Parse error at line #{}, column #{}"
         end
       end
 
